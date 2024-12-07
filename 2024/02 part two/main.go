0cc4a4f63b00e6fcddd29af4586251cef335fecd
theana <41345545+theanaverwaerde@users.noreply.github.com>
2024-12-08 00:41:14 +0100

    Advent of Code 2024 - Day 2 Part Two

---
+++ 2024/02 part two/main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	f, err := os.Open("input.txt")
+	if err != nil {
+		fmt.Print("No input.txt file")
+		return
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	safeCount := 0
+
+	for scanner.Scan() {
+		if IsSafe(scanner.Text()) {
+			safeCount++
+		}
+	}
+
+	fmt.Println("Safe reports count:", safeCount)
+}
+
+func IsSafe(s string) bool {
+	values := strings.Split(s, " ")
+
+	for i := 0; i < len(values); i++ {
+		if IsSafeList(removeElement(values, i)) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func removeElement(slice []string, i int) []string {
+	return append(append([]string{}, slice[:i]...), slice[i+1:]...)
+}
+
+func IsSafeList(values []string) bool {
+	way := DetectWay(values)
+
+	if way == 0 {
+		return false
+	}
+
+	for i := 1; i < len(values); i++ {
+		x, _ := strconv.Atoi(values[i-1])
+		y, _ := strconv.Atoi(values[i])
+
+		distance := (x - y) * way
+
+		if distance < 1 || distance > 3 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func DetectWay(values []string) int {
+	x, _ := strconv.Atoi(values[0])
+	y, _ := strconv.Atoi(values[1])
+
+	number := x - y
+
+	if number < 0 {
+		return -1
+	} else if number > 0 {
+		return 1
+	} else {
+		return 0
+	}
+}
